20168c6d66c23b4782b61a34ba998445b9232f18
KunalSangale <kunalsangale.98@gmail.com>
2021-11-18 18:34:49 +0530

    index file completed

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| QuizMania |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/quiz1.jpg) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1> QuizMania </h1>
+                <h2 style="color:white"> An Online Quiz platform to check your Knowledge </h2>
+                <a href="login.php" class="btn"> Login </a> &emsp;
+                <a href="register.php" class="btn"> Register </a>
+                <h2> Good &nbsp;Luck. </h2>
+                <h3 style="color:white"> Login as admin </h3>
+                <a href="admin.php" class="btn"> Admin Login </a>
+            </div>
+        </center>
+    </body>
+</html>
