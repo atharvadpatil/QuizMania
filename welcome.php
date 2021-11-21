5565f16b8c5059201cf9ece3bfa63163df4c3bc0
KunalSangale <kunalsangale.98@gmail.com>
2021-11-21 19:05:55 +0530

    Added the user home page.

---
+++ welcome.php
@@ -0,0 +1,111 @@
+<?php
+    include_once 'database.php';
+    session_start();
+    if(!(isset($_SESSION['email'])))
+    {
+        header("location:login.php");
+    }
+    else
+    {
+        $name = $_SESSION['name'];
+        $email = $_SESSION['email'];
+        include_once 'database.php';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome | QuizMania</title>
+    <link  rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+    <link rel="stylesheet" href="css/welcome.css">
+    <link  rel="stylesheet" href="css/font.css">
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+</head>
+<body>
+    <nav class="navbar navbar-default title1">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        <a class="navbar-brand" href="#"><b>QuizMania</b></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-left">
+            <li <?php if(@$_GET['q']==1) echo'class="active"'; ?> ><a href="welcome.php?q=1&step=3"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home<span class="sr-only">(current)</span></a></li>
+            <li <?php if(@$_GET['q']==2) echo'class="active"'; ?>> <a href="welcome.php?q=2"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>&nbsp;History</a></li>
+            <li <?php if(@$_GET['q']==3) echo'class="active"'; ?>> <a href="welcome.php?q=3"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Ranking</a></li>
+            
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+        <li <?php echo''; ?> > <a href="logout.php?q=welcome.php"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Log out</a></li>
+        </ul>
+        
+            
+           
+       
+        </div>
+    </div>
+    </nav>
+    <br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <?php if(@$_GET['q']==1 && @$_GET['step']==3) 
+                {
+                    $result = mysqli_query($con,"SELECT DISTINCT category FROM quiz ORDER BY date DESC") or die('Error');
+                    echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
+                    <tr><td><center><b>S.N.</b></center></td><td><center><b>Category</b></center></td><td><center><b>Action</b></center></td></tr>';
+                    $c=1;
+                    while($row = mysqli_fetch_array($result)) {
+                        $category = $row['category'];
+                        $category=ucfirst($category);
+                    {
+                        echo '<tr><td><center>'.$c++.'</center></td><td><center>'.$category.'</center></td><td><center><b><a href="welcome.php?q=1&cat='.$category.'" class="btn sub1" style="color:black;margin:0px;background:#1de9b6"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Select</b></span></a></b></center></td></tr>';
+                    }
+                    }
+                    $c=0;
+                    echo '</table></div></div>';
+                }?>
+                <?php if(@$_GET['q']=='1'&&!@$_GET['step']) 
+                {
+                    $category=@$_GET['cat'];
+                    $result = mysqli_query($con,"SELECT * FROM quiz WHERE category='$category' ORDER BY date DESC") or die('Error');
+                    echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
+                    <tr><td><center><b>S.N.</b></center></td><td><center><b>Topic</b></center></td><td><center><b>Total question</b></center></td><td><center><b>Marks</center></b></td><td><center><b>Preparation Link</b></center></td><td><center><b>Action</b></center></td></tr>';
+                    $c=1;
+                    while($row = mysqli_fetch_array($result)) {
+                        $title = $row['title'];
+                        $total = $row['total'];
+                        $correct = $row['correct'];
+                        $eid = $row['eid'];
+                        $link=$row['link'];
+
+                    $q12=mysqli_query($con,"SELECT score FROM history WHERE eid='$eid' AND email='$email'" )or die('Error98');
+                    $rowcount=mysqli_num_rows($q12);	
+                    if($rowcount == 0){
+                        echo '<tr><td><center>'.$c++.'</center></td><td><center>'.$title.'</center></td><td><center>'.$total.'</center></td><td><center>'.$correct*$total.'</center></td><td><center><a href="'.$link.'" target="_blank">Link</a></center></td><td><center><b><a href="welcome.php?q=quiz&step=2&eid='.$eid.'&n=1&t='.$total.'" class="btn sub1" style="color:black;margin:0px;background:#1de9b6"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Start</b></span></a></b></center></td></tr>';
+                    }
+                    else
+                    {
+                    echo '<tr style="color:#99cc32"><td><center>'.$c++.'</center></td><td><center>'.$title.'&nbsp;<span title="This quiz is already solve by you" class="glyphicon glyphicon-ok" aria-hidden="true"></span></center></td><td><center>'.$total.'</center></td><td><center>'.$correct*$total.'</center></td><td><center><a href="'.$link.'" target="_blank">Link</a></center></td><td><center><b><a href="update.php?q=quizre&step=25&eid='.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="color:black;margin:0px;background:red"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Restart</b></span></a></b></center></td></tr>';
+                    }
+                    }
+                    $c=0;
+                    echo '</table></div></div>';
+                }?>
+               
+
+</body>
+</html>
